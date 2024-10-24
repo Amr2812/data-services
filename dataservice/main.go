5f5c4be3c5a1fe0c75bb389ff243b03fe02f7581
Amr Elmohamady <amr.osama.elmohamady@gmail.com>
2024-10-24 13:04:46 +0000

    request coalescing and cassandra client gocql

--- dataservice/main.go
+++ dataservice/main.go
@@ -2,23 +2,103 @@ package main
 
 import (
 	"context"
+	"log"
 	"net"
 	"os"
-	"log"
+	"sync"
 
 	pb "github.com/Amr2812/data-services/messages"
+	"github.com/gocql/gocql"
 	"google.golang.org/grpc"
 )
+
+var session *gocql.Session
+var requestsMap *RequestsMap
+
+type RequestId struct {
+	channelId int64 // not go channel, but a channel for chat messages
+	messageId int64
+}
+
+// map of request ids to array of channels
+type RequestsMap struct {
+	mu sync.Mutex
+	// channel returns pointer to avoid a lot of copying and it won't be modified by the caller
+	requests map[RequestId][]chan *pb.MessageReply
+}
+
+func NewRequestsMap() *RequestsMap {
+	return &RequestsMap{
+		requests: make(map[RequestId][]chan *pb.MessageReply),
+	}
+}
+
+func (rm *RequestsMap) HandleRequest(requestId RequestId) *pb.MessageReply {
+	resultChan := make(chan *pb.MessageReply)
+	rm.mu.Lock()
+
+	channels, ok := rm.requests[requestId]
+	if !ok {
+		channels = make([]chan *pb.MessageReply, 0)
+		channels = append(channels, resultChan)
+		rm.requests[requestId] = channels
+		rm.mu.Unlock()
+
+		go rm.ExecuteQuery(requestId)
+	} else {
+		channels = append(channels, resultChan)
+		rm.requests[requestId] = channels
+		rm.mu.Unlock()
+	}
+
+	return <-resultChan
+}
+
+// execute the cassandra query
+// send the result to all the channels in the map
+// remove the request from the map
+func (rm *RequestsMap) ExecuteQuery(requestId RequestId) {
+	var MessageReply pb.MessageReply
+
+	err := session.Query("SELECT * FROM messages WHERE channel_id = ? AND message_id = ?", requestId.channelId, requestId.messageId).
+		Scan(&MessageReply.ChannelId, &MessageReply.MessageId, &MessageReply.AuthorId, &MessageReply.Content)
+	if err != nil {
+		log.Printf("failed to execute query: %v", err)
+	}
+
+	rm.mu.Lock()
+	channels := rm.requests[requestId]
+	delete(rm.requests, requestId)
+	rm.mu.Unlock()
+
+	log.Printf("Sending result to %d channels", len(channels))
+	for _, ch := range channels {
+		ch <- &MessageReply
+	}
+}
 
 type MessageService struct {
 	pb.UnimplementedMessagesServiceServer
 }
 
 func (s *MessageService) GetMessage(ctx context.Context, in *pb.MessageRequest) (*pb.MessageReply, error) {
-	return &pb.MessageReply{}, nil
+	requestId := RequestId{channelId: in.ChannelId, messageId: in.MessageId}
+	return requestsMap.HandleRequest(requestId), nil
 }
 
 func main() {
+	var err error
+	cluster := gocql.NewCluster("cassandra-node1")
+	cluster.Keyspace = "dataservices"
+	session, err = cluster.CreateSession()
+	if err != nil {
+		log.Fatalf("failed to connect to cassandra: %v", err)
+	}
+	log.Printf("Connected to cassandra")
+	defer session.Close()
+
+	requestsMap = NewRequestsMap()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "50051"
--- go.mod
+++ go.mod
@@ -3,13 +3,17 @@ module github.com/Amr2812/data-services
 go 1.23.1
 
 require (
+	github.com/gocql/gocql v1.7.0
 	google.golang.org/grpc v1.67.1
 	google.golang.org/protobuf v1.35.1
 )
 
 require (
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 )
